0c6bb13af29073529734ed19e8de11f6cb48d4c4
Nazar Havronskyi <nhavronskyi@gmail.com>
2025-04-11 13:15:40 +0200

    implement google gemini request

--- src/main/java/com/nhavronskyi/youtubestatsbot/service/impl/GeminiService.java
+++ src/main/java/com/nhavronskyi/youtubestatsbot/service/impl/GeminiService.java
@@ -5,17 +5,12 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.nhavronskyi.youtubestatsbot.props.GoogleProps;
+import com.nhavronskyi.youtubestatsbot.service.helpers.GoogleGeminiRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -23,45 +18,7 @@ public class GeminiService {
     private final GoogleProps googleProps;
     private final Gson gson;
 
-    @SneakyThrows
-    public String getSummaryAboutSubscribers(List<String> channelNames) {
-        String endpoint = googleProps.url().geminiUrl() + googleProps.apiKey();
-        String prompt = """
-                як ти можеш охарактеризувати людину яка підписана на усі ці канали?
-                
-                ~~~
-                %s
-                ~~~
-                тобі не потрібно охарактеризовувати кожен канал, тільки загальний підсумок.
-                convert a message to markdown format.
-                """.formatted(String.join("\n", channelNames));
-        String jsonPayload = getRequestBody(prompt);
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endpoint))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(jsonPayload))
-                .build();
-
-        HttpResponse<String> response = HttpClient.newHttpClient()
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-
-        return getAnswerFromJson(response.body());
-    }
-
-    private String getRequestBody(String promptText) {
-        Map<String, Object> payload = new HashMap<>();
-        Map<String, Object> part = new HashMap<>();
-        part.put("text", promptText);
-        Map<String, Object> contents = new HashMap<>();
-        contents.put("parts", List.of(part));
-        payload.put("contents", List.of(contents));
-
-        return gson.toJson(payload);
-    }
-
-    private String getAnswerFromJson(String json) {
+    private static String getAnswerFromJson(String json) {
         JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
 
         JsonArray candidates = jsonObject.getAsJsonArray("candidates");
@@ -72,4 +29,21 @@ public class GeminiService {
         return firstPart.get("text").getAsString();
     }
 
+    @SneakyThrows
+    public String getSummaryAboutSubscribers(List<String> channelNames) {
+        String endpoint = googleProps.url().geminiUrl() + googleProps.apiKey();
+
+        String prompt = """
+                як ти можеш охарактеризувати людину яка підписана на усі ці канали?
+                
+                ~~~
+                %s
+                ~~~
+                тобі не потрібно охарактеризовувати кожен канал, тільки загальний підсумок.
+                convert a message to markdown format.
+                """.formatted(String.join("\n", channelNames));
+        GoogleGeminiRequest googleGeminiRequest = new GoogleGeminiRequest(endpoint, prompt, gson);
+
+        return getAnswerFromJson(googleGeminiRequest.execute().body());
+    }
 }
